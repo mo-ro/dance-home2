3b65e376dd678d2a76efe8365630d0d08e1c1ba3
mo-ro <gainmoro@gmail.com>
2017-10-22 04:20:19 +0900

    SNS段まで完成

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,16 +1,32 @@
+var height = $(window).height();
+
 $(window).scroll(function() {
-  if($(window).scrollTop() >= 200) {
-    console.log($(window).scrollTop());
-    
-    $(".Index-top__navigation").css({
-      "background-color": "#183446",
-      "box-shadow": "0px 1px 2px rgba(0, 0, 0, 0.66)"
-    });
-  }else{
+  var scroll = $(window).scrollTop();
+  if(scroll <= 100) {
     $(".Index-top__navigation").css({
       "background-color": "transparent",
       "box-shadow": "none"
     });
+
+  }else if(scroll <= 300) {
+    $(".Index-top__bottom").css({
+      "bottom": "0px"
+    });
+  }else if(scroll <= height + 520){
+    $(".Index-top__navigation").css({
+      "background-color": "#F37402",
+      "box-shadow": "0px 1px 2px rgba(0, 0, 0, 0.66)"
+    });
+    $(".Index-top__bottom").css({
+      "bottom": "-80px"
+    });
+    $(".Index-top__navigation-list a").css("color", "#183446")
+  }else if(scroll <= height + 1200) {
+    $(".Index-top__navigation").css({
+      "background-color": "#03275A",
+      "box-shadow": "0px 1px 2px rgba(0, 0, 0, 0.66)",
+    });
+    $(".Index-top__navigation-list a").css("color", "#f4f4f4");
   }
   
 });
