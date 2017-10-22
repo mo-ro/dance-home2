0cbe9502b8bfb12becda334fb45c618fc40c158e
mo-ro <gainmoro@gmail.com>
2017-10-23 01:49:49 +0900

    ダンスジャンル完成

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,30 +3,41 @@ var height = $(window).height();
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
   if(scroll <= 100) {
+    
+    $(".Index-top__bottom").css({
+      "left": "0"
+    });
+    
+  }else if(scroll <= height - 150) {
     $(".Index-top__navigation").css({
       "background-color": "transparent",
       "box-shadow": "none"
     });
-
-  }else if(scroll <= 300) {
     $(".Index-top__bottom").css({
-      "bottom": "0px"
+      "left": "-100%"
     });
-  }else if(scroll <= height + 520){
+    $(".Index-top__navigation-list a").css("color", "#183446");
+  }else if(scroll <= height + 770){
     $(".Index-top__navigation").css({
       "background-color": "#F37402",
-      "box-shadow": "0px 1px 2px rgba(0, 0, 0, 0.66)"
+      "box-shadow": "0px 1px 1px rgba(0, 0, 0, 0.6)"
     });
-    $(".Index-top__bottom").css({
-      "bottom": "-80px"
-    });
-    $(".Index-top__navigation-list a").css("color", "#183446")
-  }else if(scroll <= height + 1200) {
+
+    $(".Index-top__navigation-list a").css("color", "#f4f4f4");
+  }else if(scroll <= height + 1100) {
     $(".Index-top__navigation").css({
       "background-color": "#03275A",
-      "box-shadow": "0px 1px 2px rgba(0, 0, 0, 0.66)",
     });
     $(".Index-top__navigation-list a").css("color", "#f4f4f4");
+  }else if(scroll <= height + 1900) {
+    $(".Index-top__navigation").css({
+      "background-color": "#1A7DD7",
+    });
+    // $(".Index-top__navigation-list a").css("color", "#183446");
+  }else if(scroll <= height + 3000) {
+    $(".Index-top__navigation").css({
+      "background-color": "#F2B809",
+    });
   }
   
 });
