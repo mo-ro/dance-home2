047bc9ee2e8758266eea24285381c904331f7df7
mo-ro <gainmoro@gmail.com>
2017-10-26 16:32:15 +0900

    お知らせ完成

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,26 @@
 var height = $(window).height();
+var about_images = $(".About__images");
+
+$(about_images).on("inview", function() {
+  setTimeout($(about_images).css({
+    "opacity": 1
+  }), 1000)
+})
+
+$(".Index-top__content").ready(setTimeout(function() {
+  $(".Index-top__content").css("opacity", 1)
+}, 1000))
 
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
+  
+  if($(".Index-top__logo").is(":hidden")) {
+    // $(".Index-top__logo")).css("opacity", "1")
+    console.log("aaa")
+  }else{
+    console.log("bbb")
+  }
+  
   if(scroll <= 100) {
     
     $(".Index-top__bottom").css({
@@ -17,26 +36,26 @@ $(window).scroll(function() {
       "left": "-100%"
     });
     $(".Index-top__navigation-list a").css("color", "#183446");
-  }else if(scroll <= height + 770){
+  }else if(scroll <= height + 650){
     $(".Index-top__navigation").css({
       "background-color": "#F37402",
       "box-shadow": "0px 1px 1px rgba(0, 0, 0, 0.6)"
     });
 
     $(".Index-top__navigation-list a").css("color", "#f4f4f4");
-  }else if(scroll <= height + 1100) {
+  }else if(scroll <= height + 950) {
     $(".Index-top__navigation").css({
       "background-color": "#03275A",
     });
     $(".Index-top__navigation-list a").css("color", "#f4f4f4");
   }else if(scroll <= height + 1900) {
     $(".Index-top__navigation").css({
-      "background-color": "#1A7DD7",
+      "background-color": "#F2B809",
     });
     // $(".Index-top__navigation-list a").css("color", "#183446");
-  }else if(scroll <= height + 3000) {
+  }else if(scroll <= height + 2700) {
     $(".Index-top__navigation").css({
-      "background-color": "#F2B809",
+      "background-color": "#1A7DD7",
     });
   }
   
